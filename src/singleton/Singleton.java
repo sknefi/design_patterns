c04914da302d999e7c92f382dff62ec62967c4cd
sknefi <filip.karika1@gmail.com>
2025-06-13 19:19:21 +0200

    add comment

--- src/singleton/Singleton.java
+++ src/singleton/Singleton.java
@@ -1,5 +1,7 @@
 package singleton;
 
+// Singleton allows us to have only one instance of object, if that instance was already created
+// and we try to create another one, the first instance will be returned
 public class Singleton {
 	private static Singleton instance;
 
